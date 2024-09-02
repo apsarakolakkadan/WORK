14fb631f0eeb1b3dce0a0eab92609316b47c8ae9
apsarakolakkadan <apsarakolakkadan@gmail.com>
2024-09-02 10:34:17 +0530

    first commit

---
+++ array/src/arraypkg/Arrayloop.java
@@ -0,0 +1,24 @@
+package arraypkg;
+
+public class Arrayloop
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*int i,num[]= {1,2,3,4,5,6,7,8,9,10};
+		//System.out.println(num[0]);
+		
+		for(i=0;i<num.length;i++)
+		{
+			System.out.println(num[i]);
+		} */
+		int i;
+		String name[]= {"anu","manu","sinu", "radha", "apsara", "priya"};
+		//System.out.println(name[4]);
+		
+				for(i=0;i<name.length;i++)
+				{
+					System.out.println(name[i]);
+				}
+	}
+
+}
